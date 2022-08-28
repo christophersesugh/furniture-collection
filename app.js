2ae175f8ff8499a9b3a016fb67bb19e73230bb0e
Christopher Sesugh Aondona <christohybrid185@gmail.com>
2022-08-28 12:32:59 +0100

    Products dom and local storage

--- app.js
+++ app.js
@@ -19,19 +19,70 @@ class Products {
     try {
       const result = await window.fetch("products.json");
       const data = await result.json();
-      return data;
+      let products = data.items;
+      products = products.map((product) => {
+        const { title, price } = product.fields;
+        const { id } = product.sys;
+        const image = product.fields.image.fields.file.url;
+        return { title, price, id, image };
+      });
+      return products;
     } catch (error) {
       console.log("Error", error);
     }
   }
 }
 
-class UI {}
+class UI {
+  displayProducts(products) {
+    let results = ``;
+    products.forEach((product) => {
+      results += `
+      <!-- single product -->
+        <article class="product">
+          <div class="img-container">
+            <img
+              src=${product.image}
+              alt="product"
+              class="product-img"
+            />
+            <button class="bag-btn" data-id=${product.id}>
+              <i class="material-icons">shopping_cart</i>
+              add to cart
+            </button>
+          </div>
+          <h3>${product.title}</h3>
+          <h4>$ ${product.price}</h4>
+        </article>
+        <!-- end of single product -->
+      `;
+    });
+    productsDOM.innerHTML = results;
+  }
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+    });
+  }
+}
 
-class Storage {}
+class Storage {
+  static saveProducts(products) {
+    window.localStorage.setItem("products", JSON.stringify(products));
+  }
+}
 
 document.addEventListener("DOMContentLoaded", (e) => {
   const products = new Products();
-  products.getProducts().then((products) => console.log(products));
   const ui = new UI();
+
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => ui.getBagButtons());
 });
